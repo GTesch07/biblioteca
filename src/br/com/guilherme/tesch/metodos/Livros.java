5984c6a10a819084074f51c29c528cffc8c59992
Guilherme da Silva Tesch <tesch.guilherme@hotmail.com>
2025-06-27 16:05:24 -0300

    Atualizando escalonamento do arquivo e realizando primeiro teste de inserção ao suporte UTF_8.

--- src/br/com/guilherme/tesch/metodos/Livros.java
+++ src/br/com/guilherme/tesch/metodos/Livros.java
@@ -1,6 +1,17 @@
 package br.com.guilherme.tesch.metodos;
 
-import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
+
+
+
 
 public class Livros {
     private String titulo;
@@ -8,8 +19,9 @@ public class Livros {
     private int paginas;
     private double preco;
     private char categoria;
+    private static final String ARQUIVO = "livros.txt";
 
-    public Livros(String titulo, String autor, int paginas, double preco, char categoria){
+    public Livros(String titulo, String autor, int paginas, double preco, char categoria) {
         this.titulo = titulo;
         this.autor = autor;
         this.paginas = paginas;
@@ -21,8 +33,8 @@ public class Livros {
         return categoria;
     }
 
-    public static String getDescricaoCategoria(char categoria){
-        return switch (categoria){
+    public static String getDescricaoCategoria(char categoria) {
+        return switch (categoria) {
             case 'F' -> "Ficção";
             case 'N' -> "Não-ficção";
             case 'T' -> "Tecnologia";
@@ -32,27 +44,62 @@ public class Livros {
     }
 
     @Override
-    public String toString(){
+    public String toString() {
         return String.format("""
                 Título: %s
                   Autor: %s
                   Páginas: %d
                   Preço: %.2f
-                """, titulo,autor,paginas,preco);
+                """, titulo, autor, paginas, preco);
     }
 
-    public String toFileFormat(){
-        return titulo + ";" + autor +";" + paginas +";"+preco+";"+categoria;
+    public String toFileFormat() {
+        return titulo + ";" + autor + ";" + paginas + ";" + preco + ";" + categoria;
     }
 
-    public static Livros fromFileFormat(String linha){
+    public static Livros fromFileFormat(String linha) {
         String[] dados = linha.split(";");
         return new Livros(
                 dados[0],
                 dados[1],
                 Integer.parseInt(dados[2]),
                 Double.parseDouble(dados[3]),
-                dados[4].charAt(0)
-        );
+                dados[4].charAt(0));
+    }
+
+    public static void salvarLivro(Livros livro) {
+        try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(ARQUIVO, true), StandardCharsets.UTF_8))) {
+            writer.write(livro.toFileFormat());
+            writer.newLine();
+        } catch (IOException e) {
+            System.out.println("Erro ao salvar livro: " + e.getMessage());
+        }
+    }
+
+    public static void listarLivrosPorCategoria(char categoria) {
+        File arquivo = new File(ARQUIVO);
+        if (!arquivo.exists()) {
+            System.out.println("Nenhum livro cadastrado ainda.");
+            return;
+        }
+        System.out.println("\nLivros da categoria " + Livros.getDescricaoCategoria(categoria) + ":");
+        boolean encontrou = false;
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(ARQUIVO),StandardCharsets.UTF_8))) {
+            String linha;
+            while ((linha = reader.readLine()) != null) {
+                Livros livro = Livros.fromFileFormat(linha);
+                if (livro.getCategoria() == categoria) {
+                    System.out.println("- " + livro);
+                    encontrou = true;
+                }
+            }
+            if (!encontrou) {
+                System.out.println("Nenhum livro encontrado nessa categoria.");
+            }
+        } catch (IOException e) {
+            System.out.println("Erro ao ler livros: " + e.getMessage());
+        }
+
     }
 }
--- src/br/com/guilherme/tesch/principal/Biblioteca.java
+++ src/br/com/guilherme/tesch/principal/Biblioteca.java
@@ -2,12 +2,12 @@ package br.com.guilherme.tesch.principal;
 
 import br.com.guilherme.tesch.metodos.Livros;
 
-import java.io.*;
+
 import java.util.Scanner;
 
 public class Biblioteca {
-    private static final String ARQUIVO = "livros.txt";
-    public static void main(String[]args){
+
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         char opcao;
 
@@ -16,7 +16,7 @@ public class Biblioteca {
             opcao = scanner.next().toUpperCase().charAt(0);
             scanner.nextLine();
 
-            if (opcao == 'S'){
+            if (opcao == 'S') {
                 System.out.print("Título: ");
                 String titulo = scanner.nextLine();
 
@@ -32,56 +32,20 @@ public class Biblioteca {
                 System.out.print("Categoria (F - Ficção, N - Não-ficção, T - Tecnoliga, A - Aventura): ");
                 char categoria = scanner.next().toUpperCase().charAt(0);
 
-                if ("FNTA".indexOf(categoria) != -1){
-                    Livros livro = new Livros(titulo,autor,paginas,preco,categoria);
-                    salvarLivro(livro);
+                if ("FNTA".indexOf(categoria) != -1) {
+                    Livros livro = new Livros(titulo, autor, paginas, preco, categoria);
+                    Livros.salvarLivro(livro);
                     System.out.println("Livro salvo com sucesso.");
-                }else {
+                } else {
                     System.out.println("Categoria inválida! Livro não cadastrado.");
                 }
             }
-        }while(opcao == 'S');
+        } while (opcao == 'S');
         System.out.print("\nDeseja ver livros de qual categoria? (F/N/T/A)\n");
         char categoriaConsulta = scanner.next().toUpperCase().charAt(0);
-        listarLivrosPorCategoria(categoriaConsulta);
+        Livros.listarLivrosPorCategoria(categoriaConsulta);
 
         scanner.close();
-    }
-    private static void salvarLivro(Livros livro){
-        try(BufferedWriter writer = new BufferedWriter(new FileWriter(ARQUIVO,true))){
-            writer.write(livro.toFileFormat());
-            writer.newLine();
-        }catch (IOException e){
-            System.out.println("Erro ao salvar livro: "+ e.getMessage());
-        }
-    }
-
-
-    private static void listarLivrosPorCategoria(char categoria){
-        File arquivo = new File(ARQUIVO);
-        if (!arquivo.exists()){
-            System.out.println("Nenhum livro cadastrado ainda.");
-            return;
-        }
-        System.out.println("\nLivros da categoria " + Livros.getDescricaoCategoria(categoria)+ ":");
-        boolean encontrou = false;
-
-        try (BufferedReader reader = new BufferedReader(new FileReader(ARQUIVO))){
-            String linha;
-            while ((linha = reader.readLine()) != null){
-                Livros livro = Livros.fromFileFormat(linha);
-                if (livro.getCategoria() == categoria){
-                    System.out.println("- " + livro);
-                    encontrou = true;
-                }
-            }
-            if (!encontrou){
-                System.out.println("Nenhum livro encontrado nessa categoria.");
-            }
-        }catch (IOException e){
-            System.out.println("Erro ao ler livros: "+ e.getMessage());
-        }
-
     }
 
 }
